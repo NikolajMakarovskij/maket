c6e147ff0dd775dca8b1bde450b883734ad001a6
NikolajMakarovskij <81770644+NikolajMakarovskij@users.noreply.github.com>
2021-12-17 13:05:40 +0300

    advantagws fix partners JS fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".slider-header", {
+const swiper1 = new Swiper(".slider-header", {
   
   loop: true,
   autoplay: {
@@ -16,6 +16,25 @@ var swiper = new Swiper(".slider-header", {
     type: 'bullets',
     clickable: true,
   },
+  mousewheel: true,
+  keyboard: true,
+});
+ 
+const swiper2 = new Swiper(".slider-partners", {
+  slidesPerView: 6,
+        spaceBetween: 17,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+  
+
+  
+  speed: 1000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
   mousewheel: true,
   keyboard: true,
 });
